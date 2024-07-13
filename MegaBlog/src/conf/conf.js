62b2a031403532d4f9ed992d7659fa6664dc0a98
Dipesh <dipeshpatro@gmail.com>
2024-07-14 02:52:57 +0530

    uff

--- MegaBlog/src/conf/conf.js
+++ MegaBlog/src/conf/conf.js
@@ -1,5 +1,5 @@
 const conf = {
-    appwriteURl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteURL: String(import.meta.env.VITE_APPWRITE_URL),
     appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
     appwriteDatabaseId: String(import.meta.env.VITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.VITE_COLLECTION_ID),
